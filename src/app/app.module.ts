a776e988cf68210da94fa01fff21e2c16612feeb
RaulSanchezG <raulsanchezgranda@gmail.com>
2024-04-16 16:53:41 +0200

    Hasta video 66

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/components/counter/counter.module';
 import { HeroesModule } from './heroes/hero/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 
 
@@ -14,7 +15,8 @@ import { HeroesModule } from './heroes/hero/heroes.module';
   imports: [
     BrowserModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
 
   ],
   providers: [],
---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent {
+
+  public characters: Character[]=[{
+    name:'Goku',
+    power:9500
+  },{
+    name: 'Krillin',
+    power:1000
+  },{
+    name:'Vegeta',
+    power:7500
+
+
+  }];
+
+
+  }
+
+
+
+
